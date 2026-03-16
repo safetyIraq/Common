cf63e7f0b087c3c51f500e36b467edf515785d89
safetyIraq <paidhydr@gmail.com>
2026-03-16 10:36:36 +0300

    Update User.java

--- app/src/main/java/com/example/emptyactivity/User.java
+++ app/src/main/java/com/example/emptyactivity/User.java
@@ -1,6 +1,11 @@
 package com.example.emptyactivity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class User {
+    
+    // المتغيرات
     private String uid;
     private String displayName;
     private String username;
@@ -8,9 +13,13 @@ public class User {
     private String bio;
     private String email;
 
-    public User() {}
+    // مُشيد فارغ (مهم لـ Firebase)
+    public User() {
+    }
 
-    public User(String uid, String displayName, String username, String profileImage, String bio, String email) {
+    // مُشيد كامل
+    public User(String uid, String displayName, String username, 
+                String profileImage, String bio, String email) {
         this.uid = uid;
         this.displayName = displayName;
         this.username = username;
@@ -19,10 +28,69 @@ public class User {
         this.email = email;
     }
 
-    public String getUid() { return uid != null ? uid : ""; }
-    public String getDisplayName() { return displayName != null ? displayName : "مستخدم"; }
-    public String getUsername() { return username != null ? username : ""; }
-    public String getProfileImage() { return profileImage != null ? profileImage : ""; }
-    public String getBio() { return bio != null ? bio : ""; }
-    public String getEmail() { return email != null ? email : ""; }
+    // Getters مع التحقق من null (هذا هو الحل)
+    @NonNull
+    public String getUid() { 
+        return uid != null ? uid : ""; 
+    }
+
+    @NonNull
+    public String getDisplayName() { 
+        return displayName != null ? displayName : "مستخدم"; 
+    }
+
+    @NonNull
+    public String getUsername() { 
+        return username != null ? username : ""; 
+    }
+
+    @NonNull
+    public String getProfileImage() { 
+        return profileImage != null ? profileImage : ""; 
+    }
+
+    @NonNull
+    public String getBio() { 
+        return bio != null ? bio : ""; 
+    }
+
+    @NonNull
+    public String getEmail() { 
+        return email != null ? email : ""; 
+    }
+
+    // Setters
+    public void setUid(String uid) { 
+        this.uid = uid; 
+    }
+
+    public void setDisplayName(String displayName) { 
+        this.displayName = displayName; 
+    }
+
+    public void setUsername(String username) { 
+        this.username = username; 
+    }
+
+    public void setProfileImage(String profileImage) { 
+        this.profileImage = profileImage; 
+    }
+
+    public void setBio(String bio) { 
+        this.bio = bio; 
+    }
+
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
